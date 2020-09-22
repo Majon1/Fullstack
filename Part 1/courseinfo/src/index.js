0c055ef7165f61a093647dc7ce9abe91a2f9089d
Majon1 <mathilda.nynas@gmail.com>
2020-09-22 10:19:09 +0300

    ex1.2

--- Part 1/courseinfo/src/index.js
+++ Part 1/courseinfo/src/index.js
@@ -12,33 +12,61 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>{props.content} {props.amount}</p>
+      <Part1 contentpart1={props.part1} exercises={props.exercises1} />
+      <Part2 contentpart2={props.part2} exercises={props.exercises2} />
+      <Part3 contentpart3={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
+const Part1 = (props) => {
+  return (
+    <div>
+      <p> {props.contentpart1} {props.exercises}</p>
+    </div>
+  )
+}
+const Part2 = (props) => {
+  return (
+    <div>
+      <p> {props.contentpart2}  {props.exercises}</p>
+    </div>
+  )
+}
+const Part3 = (props) => {
+  return (
+    <div>
+      <p> {props.contentpart3} {props.exercises}</p>
+    </div>
+  )
+}
+
 const Total = (props) => {
   return (
     <div>
       <p>{props.total}</p>
     </div>
   )
-  }
+}
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
   return (
     <div>
       <Header course={course} />
-      <Content content={part1} amount={exercises1} />
-      <Content content={part2} amount={exercises2} />
-      <Content content={part3} amount={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+
     </div>
   )
 }
