4ef5ac0b65c74b491cc190b454173352903aaa03
Majon1 <mathilda.nynas@gmail.com>
2020-09-24 19:50:59 +0300

    table test

--- Part 1/unicafe/src/index.js
+++ Part 1/unicafe/src/index.js
@@ -9,60 +9,50 @@ const Statistics = (props) => {
   }
   return (
     <div>
-      <table>
-        <tbody>
-        <tr>
-       <Statistic text='good' value ={props.pos}/>
-      </tr>
-      <tr>
-     <Statistic text='neutral' value ={props.neu}/>
-      </tr>
-      <tr>
-      <Statistic text='bad' value ={props.neg}/>
-      </tr>
-      <tr>
-      <Statistic text='all' value ={props.all}/>
-      </tr>
-      <tr>
-      <Statistic text='average' value ={props.avg}/>
-      </tr>
-      <tr>
-      <Statistic text='positive' value ={props.proc}/>
-      </tr>
-    </tbody>
-    </table>
+      <Statistic text='good' value={props.pos} />
+      <Statistic text='neutral' value={props.neu} />
+      <Statistic text='bad' value={props.neg} />
+      <Statistic text='all' value={props.all} />
+      <Statistic text='average' value={props.avg} />
+      <Statistic text='positive' value={props.proc} />
     </div>
   )
 }
 const Statistic = (props) => {
   return (
-   <div>
-      <td>{props.text}</td> <td>{props.value}</td>
- </div> )
-}
-   /* <table>
-      <tbody>
-        <tr>
-          <td> good </td><td> {props.pos}</td>
-        </tr>
-        <tr>
-          <td>neutral </td><td>{props.neu}</td>
-        </tr>
-        <tr>
-          <td>bad </td><td>{props.neg}</td>
-        </tr>
-        <tr>
-          <td>all </td><td>{props.all}</td>
-        </tr>
-        <tr>
-          <td>average</td><td> {(props.pos - props.neg) / props.all}</td>
-        </tr>
-        <tr>
-          <td>positive</td><td> {props.pos / props.all * 100}%</td>
-        </tr>
+    <div>
+      <table>
+        <tbody>
+          <tr>
+         <td> {props.text} </td>  <td>{props.value}</td>
+          </tr>
         </tbody>
-    </table> */
-  
+      </table>
+    </div>)
+}
+/* <table>
+   <tbody>
+     <tr>
+       <td> good </td><td> {props.pos}</td>
+     </tr>
+     <tr>
+       <td>neutral </td><td>{props.neu}</td>
+     </tr>
+     <tr>
+       <td>bad </td><td>{props.neg}</td>
+     </tr>
+     <tr>
+       <td>all </td><td>{props.all}</td>
+     </tr>
+     <tr>
+       <td>average</td><td> {(props.pos - props.neg) / props.all}</td>
+     </tr>
+     <tr>
+       <td>positive</td><td> {props.pos / props.all * 100}%</td>
+     </tr>
+     </tbody>
+ </table> */
+
 
 const Button = ({ onClick, text }) => (
   <button onClick={onClick}>
@@ -83,7 +73,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button onClick={() => setBad(bad + 1)} text='bad' />
       <h2>Statistics</h2>
-      <Statistics pos={good} neu={neutral} neg={bad} all={good + neutral + bad} avg={(good-bad)/(good + neutral + bad)} proc={good/(good + neutral + bad)* 100}/>
+      <Statistics pos={good} neu={neutral} neg={bad} all={good + neutral + bad} avg={(good - bad) / (good + neutral + bad)} proc={good / (good + neutral + bad) * 100} />
     </div>
   )
 
