b013285f558d1a588bc93a6afdd4ed4f04841749
Mathilda Nynäs SOFTK20P <mathilda.nynas@gmail.com>
2020-09-24 14:17:45 +0300

    exercises

--- Part2/testingnotes/src/App.js
+++ Part2/testingnotes/src/App.js
@@ -1,11 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('')
-  const[showAll, setShowAll] = useState(true)
+  const [notes, setNotes] = useState([]) //for empty list useState([])
+  const [newNote, setNewNote] = useState('') // content of textbox
+  const [showAll, setShowAll] = useState(true)
 
+  useEffect(() => {
+    console.log('effect')
+  
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    }
+  
+    const promise = axios.get('http://localhost:3001/notes')
+    promise.then(eventHandler)
+  }, []) //<<<<< DEPENDENCY ARRAY!!!!
+
+  /*useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, []) */////<<<---DEPENDENCY ARRAY IS THIS >>>[]<<<< 
+  console.log('render', notes.length, 'notes')
+
+  //reflects current value of input, now we can create new notes!!
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
@@ -18,34 +43,40 @@ const App = (props) => {
     setNewNote('')
   }
 
+  //inside app since it's only called from app! MAKES TEXT SHOW UP IN CONSOLE!
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
   }
+  //stores a list of all the notes to be displayed
+  //IF ELSE!!!!!! 
   const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important)
+    ? notes //if showAll = true
+    : notes.filter(note => note.important) //else
+  //IF ELSE!!!!!!!!!!!!
+  //Filter works as map, creates an array
 
   return (
     <div>
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? 'important' : 'all'}
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => 
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote}
-        onChange={handleNoteChange}
+          onChange={handleNoteChange}
         />
         <button type='submit'>save</button>
       </form>
-    </div>
+    </div>//form gives box and buttons
+    //sethowAll
   )
 }
 
