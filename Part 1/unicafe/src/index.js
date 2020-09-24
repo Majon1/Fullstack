485376c815d17aa36759bce56de10887c7b141bb
Mathilda Nynäs SOFTK20P <mathilda.nynas@gmail.com>
2020-09-24 09:58:08 +0300

    unicafe fixing

--- Part 1/unicafe/src/index.js
+++ Part 1/unicafe/src/index.js
@@ -8,7 +8,39 @@ const Statistics = (props) => {
     )
   }
   return (
-    <table>
+    <div>
+      <table>
+        <tbody>
+        <tr>
+       <Statistic text='good' value ={props.pos}/>
+      </tr>
+      <tr>
+     <Statistic text='neutral' value ={props.neu}/>
+      </tr>
+      <tr>
+      <Statistic text='bad' value ={props.neg}/>
+      </tr>
+      <tr>
+      <Statistic text='all' value ={props.all}/>
+      </tr>
+      <tr>
+      <Statistic text='average' value ={props.avg}/>
+      </tr>
+      <tr>
+      <Statistic text='positive' value ={props.proc}/>
+      </tr>
+    </tbody>
+    </table>
+    </div>
+  )
+}
+const Statistic = (props) => {
+  return (
+   <div>
+      <td>{props.text}</td> <td>{props.value}</td>
+ </div> )
+}
+   /* <table>
       <tbody>
         <tr>
           <td> good </td><td> {props.pos}</td>
@@ -29,9 +61,9 @@ const Statistics = (props) => {
           <td>positive</td><td> {props.pos / props.all * 100}%</td>
         </tr>
         </tbody>
-    </table> 
-  )
-}
+    </table> */
+  
+
 const Button = ({ onClick, text }) => (
   <button onClick={onClick}>
     {text}
@@ -51,7 +83,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button onClick={() => setBad(bad + 1)} text='bad' />
       <h2>Statistics</h2>
-      <Statistics pos={good} neu={neutral} neg={bad} all={good + neutral + bad} />
+      <Statistics pos={good} neu={neutral} neg={bad} all={good + neutral + bad} avg={(good-bad)/(good + neutral + bad)} proc={good/(good + neutral + bad)* 100}/>
     </div>
   )
 
