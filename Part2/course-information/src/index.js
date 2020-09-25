4cf613d9c5b8df5612fd8f6e8f9ac5702e37cb7b
Majon1 <mathilda.nynas@gmail.com>
2020-09-25 16:11:58 +0300

    ex 2.5 done

--- Part2/course-information/src/index.js
+++ Part2/course-information/src/index.js
@@ -1,50 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Course from './components/Course'
 
-const Course = (props) => {
-  return (
-    <div>
-      <Header courseN={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total amount={props.course.parts}/>
-    </div>
-  )
-}
-
-const Header = (props) => {
-  console.log('header', props);
-  return (
-    <div>
-      <h2><p>{props.courseN}</p></h2>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  console.log('content', props);
-  return (
-    <div>
-      <Parts parts={props.parts} />
-    </div>
-  )
-}
-
-const Parts = (props) => {
-  console.log('parts print', props);
-    const cparts = props.parts.map(part => <p key={part.id}>
-    {part.name + ' ' + part.exercises}</p>)
-      return (
-       cparts)
-}
-
-const Total = (props) => {
-  console.log('total prints', props);
-  const total = props.amount.reduce((sum, order) => {
-    console.log('what happens', sum, order)
-   return sum + order.exercises
-   }, 0)
-   return (<h4><p> total of {total} exercises</p></h4> )
-}
 
 const App = () => {
   const courses = [
