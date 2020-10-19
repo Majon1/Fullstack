8d19cae59500989145a8766e66b74b612556f9f4
Majon1 <mathilda.nynas@gmail.com>
2020-10-19 14:23:55 +0300

    ex4.4 done

--- Part4/Bloglist/utils/list_helper.js
+++ Part4/Bloglist/utils/list_helper.js
@@ -1,7 +1,28 @@
 const dummy = (blogs) => {
-return 1
+    return 1
+}
+const totalLikes = (array) => {
+    if (array.length === 0)
+    {  return array.length === 0
+        ? 0
+        : array.reduce(reducer, 0) / array.length
+    }
+    if (array.length === 1)
+    {
+        return array[0].likes
+    }
+    if (array.length > 1)
+    {
+        let sum = 0
+        for (let i = 0; i < array.length; i++) 
+        {
+        sum += array[i].likes
+        }
+        return sum
+    }
 }
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
 }
