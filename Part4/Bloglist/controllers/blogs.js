4de4bc351accc8410a0a5b6526e0b0eaac0dae6b
Majon1 <mathilda.nynas@gmail.com>
2020-10-28 14:13:26 +0200

    ex4.13-14 can update and delete blogposts

--- Part4/Bloglist/controllers/blogs.js
+++ Part4/Bloglist/controllers/blogs.js
@@ -2,9 +2,9 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
-    response.json(blogs)
-  })
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
 
 
 blogsRouter.get('/:id', (request, response, next) => {
@@ -37,6 +37,23 @@ blogsRouter.delete('/:id', (request, response, next) => {
   Blog.findByIdAndRemove(request.params.id)
     .then(() => {
       response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON())
     })
     .catch(error => next(error))
 })
